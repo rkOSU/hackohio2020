8e85481c768c734948ea9a16ed791e0eac27fd96
rkOSU <46794263+rkOSU@users.noreply.github.com>
2020-11-14 00:52:37 -0500

    apple

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO RAJ</Text>
+      <Text>HELLO SUJAN</Text>
       <StatusBar style="auto" />
     </View>
   );
